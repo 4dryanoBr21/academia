cd4fcc6e1909d7d27f71443045777963e4f3ac16
Adriano Canto Junior <adrianocanto462@gmail.com>
2026-01-08 17:56:47 -0300

    08/01 | 17:56

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import usuarios.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', usuarios.views.login),
 ]
